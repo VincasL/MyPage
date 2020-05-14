24bb2ea86cc6cc51f9fcca0f76dd0da1fb6bbe5e
VincasL <vincas.linkevicius@gmail.com>
2020-05-14 21:46:57 +0300

    Added dark mode for index page

--- script/script.js
+++ script/script.js
@@ -2,3 +2,19 @@ function OpenLink(link)
 {
     window.location = link;
 }
+function ToggleDarkMode()
+{
+    var checkBox = document.getElementById("toggle-dark-mode");
+    if(checkBox.checked == true)
+    {
+        document.body.style.background = "background: rgb(24,33,22)"
+        document.body.style.background += "linear-gradient(90deg, rgba(24,33,22,1) 0%, rgba(20,16,16,1) 50%, rgba(61,43,17,1) 100%)";
+
+        document.body.style.color = "white";
+    }
+    else
+    {
+        document.body.style.background = "";
+        document.body.style.color = "";
+    }
+}
